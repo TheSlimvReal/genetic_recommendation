6ed6e8970652ccefe05f818aa6af735b1377c7fc
Simon <therealslimv@yahoo.de>
2020-06-02 20:45:21 +0300

    finished code for b4

--- cf.py
+++ cf.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 
 PROBABILITY_OF_CROSSOVER = 0.9
@@ -13,30 +14,20 @@ NUM_MOVIES = 1682
 
 rng = np.random.default_rng()
 
-ratings = np.genfromtxt('ml-100k/u.data', dtype='int32')
-matrix = np.zeros((NUM_USERS, NUM_MOVIES), dtype='int32')
+ratings_base = np.genfromtxt('ml-100k/ua.base', dtype='int32')
+ratings_test = np.genfromtxt('ml-100k/ua.test', dtype='int32')
+matrix_base = np.zeros((NUM_USERS, NUM_MOVIES), dtype='int32')
+matrix_test = np.zeros((NUM_USERS, NUM_MOVIES), dtype='int32')
 
-for r in ratings:
-    matrix[r[0]-1, r[1]-1] = r[2]
+for r in ratings_base:
+    matrix_base[r[0]-1, r[1]-1] = r[2]
 
-matrix_filled = matrix.copy()
+for r in ratings_test:
+    matrix_test[r[0] - 1, r[1] - 1] = r[2]
+
+matrix_filled = matrix_base.copy()
 unrated = np.where(matrix_filled == 0)
 matrix_filled[unrated] = np.random.randint(low=1, high=6, size=len(unrated[1]))
-
-user = matrix[0]                                    # User we are evaluating
-user_rated = np.where(user != 0)
-user_unrated = np.where(user == 0)
-others = matrix[1:]
-others_filled = matrix_filled[1:]                   # Other users with randomly filled values
-
-# Pair wise similarity with every user
-similarity = np.array([pearsonr(user[user_rated], us[user_rated])[0] for us in others_filled])
-
-max_users = similarity.argsort()[-10:][::-1]        # Top 10 most similar users
-
-neighborhood = others[max_users]                    # Neighborhood of user
-# Actually rated movies of neighborhood
-neighborhood_rated = [np.where(neigh != 0) for neigh in neighborhood]
 
 
 def fitness(x):
@@ -57,15 +48,8 @@ def crossover(parent_pairs):
     offspring = []
     for parent in parent_pairs:
         positions = np.random.randint(low=0, high=2, size=NUM_MOVIES)
-        zeros = np.where(positions == 0)
-        ones = np.where(positions == 1)
-        off_1 = np.empty(NUM_MOVIES, dtype='int32')
-        off_2 = np.empty(NUM_MOVIES, dtype='int32')
-        off_1[zeros] = parent[0][zeros]
-        off_1[ones] = parent[1][ones]
-        off_2[zeros] = parent[1][zeros]
-        off_2[ones] = parent[0][ones]
-        offspring.extend([off_1, off_2])
+        offspring.append(np.where(positions == 0, parent[0], parent[1]))
+        offspring.append(np.where(positions == 0, parent[1], parent[0]))
     return offspring
 
 
@@ -78,51 +62,106 @@ def mutation(chromosomes):
         chrom[user_rated] = user[user_rated]
 
 
-best = np.empty((MAX_GENERATIONS, 10))
-optimals = []
-generations = []
+random_users = rng.choice(NUM_USERS, 20, replace=False)
+for ind, current_user in enumerate(random_users):
+    user = matrix_base[current_user]  # User we are evaluating
+    user_rated = np.where(user != 0)
+    user_test_pos = np.where(matrix_test[current_user] != 0)
+    user_test = matrix_test[current_user][user_test_pos]
+    user_unrated = np.where(user == 0)
+    others = np.delete(matrix_base, current_user, axis=0)
+    others_filled = np.delete(matrix_filled, current_user, axis=0)  # Other users with randomly filled values
 
-for i in range(10):
-    # Create initial population
-    initial_population = np.random.randint(low=1, high=6, size=(POPULATION_SIZE, NUM_MOVIES))
-    for chromosome in initial_population:
-        chromosome[user_rated] = user[user_rated]       # Inserts users actual ratings
+    # Pair wise similarity with every user
+    similarity = np.array([pearsonr(user[user_rated], us[user_rated])[0] for us in others_filled])
 
-    next_pop = initial_population
-    next_pop_fitness = np.array([fitness(p) for p in next_pop])
-    generation = 0
-    last_leader_change = 0
-    improvement = 1
-    while generation < MAX_GENERATIONS and generation - last_leader_change < 50:
-        current_pop = next_pop
-        current_pop_fitness = next_pop_fitness
-        parents = selection(current_pop_fitness)
-        children = crossover(current_pop[parents])
-        mutation(children)
+    max_users = similarity.argsort()[-10:][::-1]  # Top 10 most similar users
 
-        next_pop = current_pop.copy()
-        next_pop[current_pop_fitness.argsort()[:len(children)]] = np.array(children)
-        next_pop_fitness = np.array([fitness(p) for p in next_pop])
-        improvement = np.sum(next_pop_fitness) / np.sum(current_pop_fitness)
-        last_leader_change = generation if max(current_pop_fitness) < max(next_pop_fitness) else last_leader_change
-        print('Run:', i, 'Generation:', generation, 'Fitness', max(next_pop_fitness), 'Improvement:', improvement)
+    neighborhood = others[max_users]  # Neighborhood of user
+    # Actually rated movies of neighborhood
+    neighborhood_rated = [np.where(neigh != 0) for neigh in neighborhood]
 
-        best[generation][i] = max(next_pop_fitness)
+    best = np.empty((MAX_GENERATIONS, 10))
+    rmse = np.empty((MAX_GENERATIONS, 10))
+    mae = np.empty((MAX_GENERATIONS, 10))
+    optimals = []
+    generations = []
+    for i in range(10):
+        # Create initial population
+        initial_population = np.random.randint(low=1, high=6, size=(POPULATION_SIZE, NUM_MOVIES))
+        for chromosome in initial_population:
+            chromosome[user_rated] = user[user_rated]       # Inserts users actual ratings
 
-        generation += 1
+        next_pop = initial_population
+        next_pop_fitness = np.array([fitness(x) for x in next_pop])
+        next_sorted_keys = next_pop_fitness.argsort()
+        generation = 0
+        last_leader_change = 0
+        earlier_performance = 0
+        finished = False
+        while not finished:
+            current_pop = next_pop
+            current_pop_fitness = next_pop_fitness
+            current_sorted_keys = next_sorted_keys
+            parents = selection(current_pop_fitness)
+            children = crossover(current_pop[parents])
+            mutation(children)
 
-    best[generation:, i] = max(next_pop_fitness)
-    optimals.append(max(next_pop_fitness))
-    generations.append(generation)
+            next_pop = current_pop.copy()
+            next_pop[current_sorted_keys[:len(children)]] = np.array(children)
+            next_pop_fitness = np.array([fitness(x) for x in next_pop])
+            next_sorted_keys = next_pop_fitness.argsort()
+            improvement = np.sum(next_pop_fitness) / np.sum(current_pop_fitness)
+            best_sol = next_pop[next_sorted_keys[-1]]
+            best_fitness = next_pop_fitness[next_sorted_keys[-1]]
+            last_leader_change = generation if current_pop_fitness[current_sorted_keys[-1]] < best_fitness else last_leader_change
 
-print('Average optimal', np.average(np.array(optimals)))
-print('Average generations', np.average(np.array(generations)))
+            best[generation][i] = best_fitness
+            rmse[generation][i] = np.sqrt(mean_squared_error(best_sol[user_test_pos], user_test))
+            mae[generation][i] = mean_absolute_error(best_sol[user_test_pos], user_test)
 
-best_avg = np.average(best[:max(generations)], axis=1)
-plt.plot(np.arange(max(generations)), best_avg)
-plt.title('Population size: ' + str(POPULATION_SIZE) + ' Crossover: ' + str(PROBABILITY_OF_CROSSOVER) + '% Mutation: '
-          + str(PROBABILITY_OF_MUTATION) + '%')
-plt.xlabel('Number of generations')
-plt.ylabel('Average fitness of best value')
-plt.grid(b=True, axis='y')
-plt.show()
+            finished = generation > MAX_GENERATIONS or generation - last_leader_change > 50
+            if not finished and generation >= 100:
+                finished = best[generation, i] / best[generation - 100, i] < 1.01
+            generation += 1
+
+        best[generation:, i] = max(next_pop_fitness)
+        rmse[generation:, i] = rmse[generation-1, i]
+        mae[generation:, i] = mae[generation-1, i]
+        optimals.append(max(next_pop_fitness))
+        generations.append(generation)
+
+    best_avg = np.average(best[:max(generations)], axis=1)
+    rmse_avg = np.average(rmse[:max(generations)], axis=1)
+    mae_avg = np.average(mae[:max(generations)], axis=1)
+    print('RUN', ind, 'USER', current_user)
+    print('Average optimal', np.average(np.array(optimals)))
+    print('Average generations', np.average(np.array(generations)))
+    print('Average RMSE', rmse_avg[-1])
+    print('Average MAE', mae_avg[-1])
+    print('----------------------------')
+
+    # plt.plot(np.arange(max(generations)), best_avg)
+    # plt.title('Population size: ' + str(POPULATION_SIZE) + ' Crossover: ' + str(PROBABILITY_OF_CROSSOVER) + '% Mutation: '
+    #           + str(PROBABILITY_OF_MUTATION) + '%')
+    # plt.xlabel('Number of generations')
+    # plt.ylabel('Average fitness of best value')
+    # plt.grid(b=True, axis='y')
+    # plt.show()
+    #
+    # plt.plot(np.arange(max(generations)), rmse_avg)
+    # plt.title('Population size: ' + str(POPULATION_SIZE) + ' Crossover: ' + str(PROBABILITY_OF_CROSSOVER) + '% Mutation: '
+    #           + str(PROBABILITY_OF_MUTATION) + '%')
+    # plt.xlabel('Number of generations')
+    # plt.ylabel('Average RMSE of best value')
+    # plt.grid(b=True, axis='y')
+    # plt.show()
+    #
+    # plt.plot(np.arange(max(generations)), mae_avg)
+    # plt.title('Population size: ' + str(POPULATION_SIZE) + ' Crossover: ' + str(PROBABILITY_OF_CROSSOVER) + '% Mutation: '
+    #           + str(PROBABILITY_OF_MUTATION) + '%')
+    # plt.xlabel('Number of generations')
+    # plt.ylabel('Average MAE of best value')
+    # plt.grid(b=True, axis='y')
+    # plt.show()
+
